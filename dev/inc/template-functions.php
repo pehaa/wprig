bf432a29628a78e6b64a1252ed5bbaa882a4bf8d
PeHaa <info@pehaa.com>
2018-07-21 22:45:19 +0200

    project template

--- dev/inc/template-functions.php
+++ dev/inc/template-functions.php
@@ -19,7 +19,7 @@ function wprig_body_classes( $classes ) {
 
 	if ( is_active_sidebar( 'sidebar-1' ) ) {
 		global $template;
-		if ( 'custom-page-template-full.php' !== basename( $template ) ) {
+		if ( 'custom-page-template-full.php' !== basename( $template ) && 'single-project.php' !== basename( $template ) ) {
 			$classes[] = 'has-sidebar';
 		}
 	}
